6b6d308e0c44aa4ab9e4b2b7d11064f3dd185cfa
Alice <alice@Alice.local>
2019-07-19 08:02:47 -0500

    Initial file

---
+++ Epicures./Epicures./ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Epicures.
+//
+//  Created by Alice on 7/19/19.
+//  Copyright © 2019 Mallory. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
